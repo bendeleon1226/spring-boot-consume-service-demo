c3e2d5670282cef8f2571ce38854f80eb88431be
Ben De Leon <bendeleonjr@hotmail.com>
2021-09-19 22:19:13 +0800

    Initial commit - Consuming REST service using SpringBoot

---
+++ src/test/java/com/bendeleon/springbootconsumeservicedemo/SpringBootConsumeServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendeleon.springbootconsumeservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsumeServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
